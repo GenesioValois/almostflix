fd1a1370fe9e52f543f5fd44f63ad35a9fea70b5
Genésio Valois <genas96@gmail.com>
2018-05-21 21:30:22 -0300

    adding serie model

---
+++ db/migrate/20180522003008_create_series.rb
@@ -0,0 +1,16 @@
+class CreateSeries < ActiveRecord::Migration[5.2]
+  def change
+    create_table :series do |t|
+      t.string :title
+      t.string :description
+      t.string :text
+      t.string :thumbnail_key
+      t.string :category
+      t.string :references
+      t.string :featured_thumbnail_key
+      t.string :thumbnail_cover_key
+
+      t.timestamps
+    end
+  end
+end
